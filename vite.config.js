3ec6bbd00c5aa17742e25f676cd67d38d6b40821
CarlosRicardo06 <cricardoreino32@correo.unicordoba.edu.co>
2025-06-05 10:52:17 -0500

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Restaurante-Alva-rril/",
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
